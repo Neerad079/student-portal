e0c45a34a3581cad9db6cc768e1cd520695e6ae8
NEERAD <neerad_2024bcse079@nitsri.ac.in>
2026-04-30 05:25:23 +0530

    feat: Added frontend login and register pages

---
+++ studentportal/src/main/resources/static/login.js
@@ -0,0 +1,23 @@
+
+// async function :-
+document.querySelector("form").addEventListener("submit", async function(e) {
+    e.preventDefault();  // stop page reload
+
+    const username = document.getElementById("text").value;
+    const password = document.getElementById("pass").value;
+
+    const response = await fetch("http://localhost:8080/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("token", data.token);
+        alert("Login successful ✅");
+        window.location.href = "dashboard.html";  // redirect after login
+    } else {
+        alert("Invalid username or password");
+    }
+});
